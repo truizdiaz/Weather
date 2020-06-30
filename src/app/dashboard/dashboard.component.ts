6761ddaa603bd150688b939e1ef4d946c2692a0f
truizdiaz <tomiruizdiaz@gmail.com>
2020-06-30 15:26:10 -0300

    App finalizada

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,9 +9,9 @@ import { ClimaService } from '../services/clima.service';
 export class DashboardComponent implements OnInit {
   urlImage = 'https://image.flaticon.com/icons/png/512/1116/1116453.png';
   city = '';
-  temperature: string;
-  humedity: string;
-  wheader: string;
+  temperature: number;
+  humidity: number;
+  weather: string;
   
   query = false;
   constructor(private climaService: ClimaService) { }
@@ -22,7 +22,14 @@ export class DashboardComponent implements OnInit {
   getWheater() {
     this.climaService.getClima(this.city).subscribe(data => {
       console.log(data);
+      this.temperature = data.main.temp - 273;
+      this.humidity = data.main.humidity;
+      this.weather = data.weather[0].main;
       this.query = true;
+    }, error => {
+      alert('Ocurrio un error');
+      this.city = '';
+      this.query = false;
     });
   }
 
