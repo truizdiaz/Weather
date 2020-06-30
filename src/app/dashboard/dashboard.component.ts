7aba430c277cf25c64c492cc28771c7e90481a61
truizdiaz <tomiruizdiaz@gmail.com>
2020-06-30 11:36:28 -0300

    Dashboard Component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  urlImage = 'https://image.flaticon.com/icons/png/512/1116/1116453.png';
   constructor() { }
 
   ngOnInit(): void {
