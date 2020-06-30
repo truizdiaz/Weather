1ef06edfdcf6e35e850eea0002e4f7a81c47ecff
truizdiaz <tomiruizdiaz@gmail.com>
2020-06-30 11:05:15 -0300

    Creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
