70490ca47edc4c377a2be919750b1b4b503a968b
truizdiaz <tomiruizdiaz@gmail.com>
2020-06-30 12:08:33 -0300

    Consumiendo Servicio

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ClimaService } from '../services/clima.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
   urlImage = 'https://image.flaticon.com/icons/png/512/1116/1116453.png';
-  constructor() { }
+  city = '';
+  temperature: string;
+  humedity: string;
+  wheader: string;
+  
+  query = false;
+  constructor(private climaService: ClimaService) { }
 
   ngOnInit(): void {
+  }
+
+  getWheater() {
+    this.climaService.getClima(this.city).subscribe(data => {
+      console.log(data);
+      this.query = true;
+    });
   }
 
 }
--- src/app/services/clima.service.ts
+++ src/app/services/clima.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClimaService {
-  url = 'api.openweathermap.org/data/2.5/weather?&appid=';
+  url = 'http://api.openweathermap.org/data/2.5/weather?&appid=';
   key = '353e035c1f2cb617bb52a234fd18f7f8';
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getClima(ciudad: string) {
-
+  getClima(ciudad: string): Observable<any> {
+    return this.http.get(this.url + this.key + '&q=' + ciudad);
   }
 }
